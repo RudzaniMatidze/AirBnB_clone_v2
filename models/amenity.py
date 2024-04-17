e8807444ed3de17c65440ec40e0f9bcb5ae58f4a
RudzaniMatidze <rdzmatidze@gmail.com>
2024-04-17 05:15:52 -0700

    file updated

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+import os
+from sqlalchemy import relationship
+from sqlalchemy import Column, String
+from models.base_model import BaseModel, Base
+from models.place import Place_amenity
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    """ Represents anemity data set """
+    __tablename__ = 'amenities'
+    name = Column(String(128), nullable=False)
+    place_amenities = relationship("place", secondary=place_amenity)
