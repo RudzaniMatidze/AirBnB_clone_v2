8d501118da08cf9cd1fa306e8912d5c2a0ae056d
RudzaniMatidze <rdzmatidze@gmail.com>
2024-04-17 11:43:45 -0700

    file updated

--- models/amenity.py
+++ models/amenity.py
@@ -1,26 +1,22 @@
 #!/usr/bin/python3
-"""This is the amenity class"""
-from os import getenv
+""" holds class Amenity"""
+import models
 from models.base_model import BaseModel, Base
-from sqlalchemy.orm import relationship
+from os import getenv
+import sqlalchemy
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
 class Amenity(BaseModel, Base):
-    """This is the class for Amenity
-    Attributes:
-        name: input name
-    """
-    __tablename__ = 'amenities'
-    name = Column(String(128), nullable=False)
+    """Representation of Amenity """
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        __tablename__ = 'amenities'
+        name = Column(String(128),
+                      nullable=False)
+    else:
+        name = ""
 
-    @staticmethod
-    def get_place_amenities():
-        from models.place import Place  # Lazy import inside a method
-        if getenv("HBNB_TYPE_STORAGE") == "db":
-            return relationship('Place',
-                                secondary='place_amenity',
-                                back_populates='amenities')
-        else:
-            return None
-
+    def __init__(self, *args, **kwargs):
+        """initializes Amenity"""
+        super().__init__(*args, **kwargs)
