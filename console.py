2b44abdeabecfca73ddc17a06104a41fa7ca4b9a
RudzaniMatidze <rdzmatidze@gmail.com>
2024-04-17 16:53:26 -0700

    file upgraded

--- console.py
+++ console.py
@@ -73,7 +73,7 @@ class HBNBCommand(cmd.Cmd):
                 pline = pline[2].strip()  # pline is now str
                 if pline:
                     # check for *args or **kwargs
-                    if pline[0] is '{' and pline[-1] is '}'\
+                    if pline[0] == '{' and pline[-1] == '}'\
                             and type(eval(pline)) is dict:
                         _args = pline
                     else:
@@ -118,47 +118,31 @@ class HBNBCommand(cmd.Cmd):
         if not args:
             print("** class name missing **")
             return
-		# check if args contain parameters
-        p_dict = {}
-        if ('=' in args and ' ' in args):
-            # let's separate args into class & parameters
-            args = args.partition(' ')
-            class_name = args[0]
-            params = args[2].split(' ')
-            # convert params into a dictionary
 
-            for item in params:
-                param_key, param_value = tuple(item.split('='))
-                if param_value[0] == '"':
-                    param_value = param_value.strip('"').replace("_", " ")
-                    # print(type(param_value))
-                else:
-                    try:
-                        param_value = eval(param_value)
-                        # print(type(param_value))
-                    except (SyntaxError, NameError):
-                        continue
-                
-                p_dict[param_key] = param_value
-            # print(p_dict)
-
-        else:
-            class_name = args
-
+        # Parse the arguments
+        class_name, *params = args.split()
         if class_name not in HBNBCommand.classes:
             print("** class doesn't exist **")
             return
 
-        new_instance = HBNBCommand.classes[class_name]()
-        storage.save()
+        # Create a dictionary of parameters
+        p_dict = {}
+        for param in params:
+            key, value = param.split('=')
+            if value[0] == '"' and value[-1] == '"':
+                value = value[1:-1].replace('_', ' ')
+            else:
+                try:
+                    value = eval(value)
+                except (SyntaxError, NameError):
+                    continue
+            p_dict[key] = value
+
+        # Create an instance of the specified class
+        new_instance = HBNBCommand.classes[class_name](**p_dict)
+        new_instance.save()
         print(new_instance.id)
-        
-        if len(p_dict) > 0:
-            args = f"{class_name} {new_instance.id} {p_dict}"
-            # call the update method
-            self.do_update(args)
-            # print(args)
-        storage.save()
+ 
 
     def help_create(self):
         """ Help information for the create method """
@@ -308,7 +292,7 @@ class HBNBCommand(cmd.Cmd):
                 args.append(v)
         else:  # isolate args
             args = args[2]
-            if args and args[0] is '\"':  # check for quoted arg
+            if args and args[0] == '\"':  # check for quoted arg
                 second_quote = args.find('\"', 1)
                 att_name = args[1:second_quote]
                 args = args[second_quote + 1:]
@@ -316,10 +300,10 @@ class HBNBCommand(cmd.Cmd):
             args = args.partition(' ')
 
             # if att_name was not quoted arg
-            if not att_name and args[0] is not ' ':
+            if not att_name and args[0] != ' ':
                 att_name = args[0]
             # check for quoted val arg
-            if args[2] and args[2][0] is '\"':
+            if args[2] and args[2][0] == '\"':
                 att_val = args[2][1:args[2].find('\"', 1)]
 
             # if att_val was not quoted arg
