245e1c0dbbead743abd7dd77ed4aca2bcfbe3ea6
RudzaniMatidze <rdzmatidze@gmail.com>
2024-04-18 07:12:09 -0700

    file updated

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """ Module for testing file storage"""
+import os
 import unittest
 from models.base_model import BaseModel
 from models import storage
-import os
+
 
 @unittest.skipIf(os.getenv('HBNB_TYPE_STORAGE') == 'db',
                  'fileStorage test not supported')
