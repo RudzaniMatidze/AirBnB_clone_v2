9eba85a7b0a48b35e08f77075a089abd7368dc9f
RudzaniMatidze <rdzmatidze@gmail.com>
2024-04-18 17:25:02 -0700

    file updated

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,7 +8,7 @@ from models.amenity import Amenity
 from models.base_model import BaseModel
 from models.city import City
 from models.place import Place
-from models.state import state
+from models.state import State
 from models.review import Review
 from models.user import User
 
