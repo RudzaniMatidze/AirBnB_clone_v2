63fa7f2c05f06c57e16dae70d04e3fdca5419e3f
RudzaniMatidze <rdzmatidze@gmail.com>
2024-05-19 12:02:33 -0700

    file updated

--- web_flask/100-hbnb.py
+++ web_flask/100-hbnb.py
@@ -1,26 +1,28 @@
 #!/usr/bin/python3
-"""Script that starts a Flask web application"""
+"""
+Start Flask application for HBNB project
+"""
+from flask import Flask, render_template
 from models import storage
-from flask import Flask
-from flask import render_template
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
 
 app = Flask(__name__)
 
-
-@app.route("/hbnb_filters", strict_slashes=False)
-def hbnb_filters():
-    """Displays the main HBnB filters HTML page."""
-    states = storage.all("State")
-    amenities = storage.all("Amenity")
-    return render_template("10-hbnb_filters.html",
-                           states=states, amenities=amenities)
-
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Display a HTML page like 8-index.html from static"""
+    states = sorted(storage.all(State).values(), key=lambda state: state.name)
+    amenities = sorted(storage.all(Amenity).values(), key=lambda amenity: amenity.name)
+    places = sorted(storage.all(Place).values(), key=lambda place: place.name)
+    return render_template('100-hbnb.html', states=states, amenities=amenities, places=places)
 
 @app.teardown_appcontext
-def teardown(exc):
-    """Removes the current SQLAlchemy session."""
+def teardown_db(exception):
+    """Close the storage on teardown"""
     storage.close()
 
-
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host='0.0.0.0', port=5000)
