3a8dfa904cdfd62bfe6d4d2ff84fa0e4655b37dd
RudzaniMatidze <rdzmatidze@gmail.com>
2024-05-19 06:32:30 -0700

    file updated

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -18,8 +18,8 @@ def close(self):
 def cities_by_states():
     """Displays html page with states and cities"""
     states = storage.all(State)
-    return render_tempplate('8-cities_by_states.html', states=states)
+    return render_template('8-cities_by_states.html', states=states)
 
 
-if __name__ == "__Main__":
-    app.run(host='0.0.0.0', port="5000")
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
